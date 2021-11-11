4ec127493d162140105c0bf15d47f0ceaf5a15b4
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2021-11-12 03:17:53 +0530

    added category grp page

---
+++ lib/screen/categorygroups.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryGroups extends StatefulWidget {
+  CategoryGroups({Key? key}) : super(key: key);
+
+  @override
+  _CategoryGroupsState createState() => _CategoryGroupsState();
+}
+
+class _CategoryGroupsState extends State<CategoryGroups> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
